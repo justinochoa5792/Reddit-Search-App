1a7d98888d025968482113ac2bf4a60355172452
Justin Ochoa <just.och92@gmail.com>
2021-02-14 11:38:50 -0500

    added filter function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   const [term, setTerm] = useState("");
   const [article, setArticle] = useState([]);
+  const [limit, setLimit] = useState();
 
   const handleChange = (e) => {
     setTerm(e.target.value);
@@ -13,41 +14,49 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await Axios.get(
-      `http://www.reddit.com/search.json?q=${term}`
+      `http://www.reddit.com/search.json?q=${term}&limit=${limit}`
     );
     console.log(response.data.data.children);
     setArticle(response.data.data.children);
   };
 
   const renderArticles = () => {
-    return article.map((art) => {
+    return article.map((art, index) => {
       let image = art.preview
         ? art.preview.images[0].source.url
         : "https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg";
       return (
-        <div class="card mb-2">
-          <img class="card-img-top" src={`${image}`} alt="Cardcap" />
-          <div class="card-body">
-            <h5 class="card-title">${art.data.title}</h5>
-            <p class="card-text">{art.data.selftext}</p>
+        <div className="card mb-2" key={index}>
+          <img className="card-img-top" src={image} alt="Cardcap" />
+          <div className="card-body">
+            <h5 className="card-title">{art.data.title}</h5>
+            <p className="card-text">
+              {truncateString(art.data.selftext, 100)}
+            </p>
             <a
               href={art.data.url}
               target="_blank
           "
-              class="btn btn-primary"
+              className="btn btn-primary"
             >
               Read More
             </a>
             <hr />
-            <span class="badge badge-secondary">
+            <span className="badge badge-secondary">
               Subreddit: ${art.data.subreddit}
             </span>
-            <span class="badge badge-dark">Score: ${art.data.score}</span>
+            <span className="badge badge-dark">Score: ${art.data.score}</span>
           </div>
         </div>
       );
     });
   };
+
+  function truncateString(myString, limit) {
+    const shortened = myString.indexOf(" ", limit);
+    if (shortened == -1) return myString;
+    return myString.substring(0, shortened);
+  }
 
   return (
     <div className="App">
@@ -68,6 +77,25 @@ function App() {
                 placeholder="Search Term..."
                 onChange={handleChange}
               />
+            </div>
+            <h5 className="mt-2">Limit: </h5>
+            <div className="form-group">
+              <select
+                name="limit"
+                id="limit"
+                className="form-control"
+                onChange={(e) => {
+                  setLimit(e.target.value);
+                }}
+              >
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="25" selected>
+                  25
+                </option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+              </select>
             </div>
             <button
               type="submit"
